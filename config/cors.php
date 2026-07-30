080ed8a07396ec1bd52ea0771d2e0290ced1d4e1
Malcolm Cordova <41023438+rastafasha@users.noreply.github.com>
2026-07-30 11:26:36 -0400

    fix cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://padresappcolegio.vercel.app', 'https://app-pagos-colegio.vercel.app'],
 
     'allowed_origins_patterns' => ['*'],
 
